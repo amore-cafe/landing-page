75ff5b8514cdc7bb2d966f4c830c19776a92a1d8
Khetal Kankariya <khetal.kankariya@gmail.com>
2025-09-07 18:17:13 +0530

    added gaming zone, updated social-proof section

--- src/components/home/testimonials.svelte
+++ src/components/home/testimonials.svelte
@@ -1,11 +1,25 @@
-<script>
+<script lang="ts">
   import Carousel from "@lib/components/ui/carousel/carousel.svelte";
   import CarouselContent from "@lib/components/ui/carousel/carousel-content.svelte";
   import CarouselItem from "@lib/components/ui/carousel/carousel-item.svelte";
   import CarouselPrevious from "@lib/components/ui/carousel/carousel-previous.svelte";
   import CarouselNext from "@lib/components/ui/carousel/carousel-next.svelte";
-  import Card from "@lib/components/ui/card/card.svelte";
-  import CardContent from "@lib/components/ui/card/card-content.svelte";
+
+  import type { CarouselAPI } from "@lib/components/ui/carousel/context.js";
+
+  let api = $state<CarouselAPI>();
+
+  const count = $derived(api ? api.scrollSnapList().length : 0);
+  let current = $state(0);
+
+  $effect(() => {
+    if (api) {
+      current = api.selectedScrollSnap() + 1;
+      api.on("select", () => {
+        current = api!.selectedScrollSnap() + 1;
+      });
+    }
+  });
 
   const testimonials = [
     {
@@ -33,40 +47,78 @@
       author: "Neha M.",
     },
   ];
+
+  let screensize: "sm" | "md" = $state("sm");
+
+  $effect(() => {
+    if (window.innerWidth >= 768) {
+      screensize = "md";
+    }
+  });
 </script>
 
-<Carousel class="flex w-screen md:w-auto px-4 md:px-0">
-  <CarouselContent class="flex flex-col gap-4 md:flex-row h-max">
-    {#each testimonials as testimonial}
-      <CarouselItem class="md:basis-1/3">
-        <div class="testimonial-card md:min-w-48">
+{#if screensize === "md"}
+  <Carousel
+    setApi={(emblaApi) => (api = emblaApi)}
+    class="flex flex-col w-auto px-0 cursor-grab"
+    opts={{
+      align: "start",
+    }}
+  >
+    <CarouselContent class="flex gap-4 flex-row h-max">
+      {#each testimonials as testimonial}
+        <CarouselItem class="md:basis-1/3">
+          <div class="testimonial-card md:min-w-48 border border-dark-accent">
+            <div
+              class="testimonial-content flex flex-col justify-between h-full py-4"
+            >
+              <p class="text-lg font-bold font-inter mb-4 w-full text-wrap">
+                "{testimonial.text}"
+              </p>
+              <span class="font-bold text-dark-accent"
+                >— {testimonial.author}</span
+              >
+            </div>
+          </div></CarouselItem
+        >
+      {/each}
+    </CarouselContent>
+    <div class="text-muted-foreground py-2 text-center text-sm">
+      {current} of {count}
+    </div>
+  </Carousel>
+{:else}
+  <Carousel
+    setApi={(emblaApi) => (api = emblaApi)}
+    orientation="vertical"
+    opts={{
+      align: "start",
+    }}
+  >
+    <CarouselContent class="h-96">
+      {#each testimonials as testimonial}
+        <CarouselItem class="basis-1/2">
           <div
-            class="testimonial-content flex flex-col justify-between h-full py-4"
+            class="testimonial-card flex flex-col justify-around py-4 bg-dark-accent"
           >
-            <p class="text-lg font-bold font-inter mb-4 w-full text-wrap">
+            <p class="text-lg font-inter mb-4 w-full text-wrap text-white">
               "{testimonial.text}"
             </p>
-            <span class="font-bold text-[#996365]">— {testimonial.author}</span>
+            <span class="text-accent">— {testimonial.author}</span>
           </div>
-        </div></CarouselItem
-      >
-    {/each}
-  </CarouselContent>
-  <div>
-    <CarouselPrevious
-      class="bg-accent text-dark-accent border-2 font-bold hidden md:inline"
-    />
-    <CarouselNext
-      class="bg-accent text-dark-accent border-2 font-bold hidden md:inline"
-    />
-  </div>
-</Carousel>
+        </CarouselItem>
+      {/each}
+    </CarouselContent>
+    <div class="text-muted-foreground py-2 text-center text-sm">
+      {current} of {count}
+    </div>
+  </Carousel>
+{/if}
 
 <style>
   .testimonial-card {
     min-height: 16rem;
     padding: 1rem;
-    border: 1px solid #996365;
     border-radius: 1rem;
   }
 </style>
